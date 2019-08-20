6cd9cf067bce8eeaa1675462b9e287d0d93e3616
abhilashkumar002 <35776269+abhilashkumar002@users.noreply.github.com>
2019-08-20 16:44:41 +0530

    server done

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+
+const schema = require('./schema/schema');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://abhi:abhi1234@cluster0-qqz6h.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
+
+app.listen(4444, () => {
+  console.log("Listening on port 4444");
+})

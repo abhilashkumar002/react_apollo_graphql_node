a1e1e43e69571748400b809e9e15fb8f4332180d
abhilashkumar002 <35776269+abhilashkumar002@users.noreply.github.com>
2019-08-23 14:54:16 +0530

    one more commit to go

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const schema = require('./schema/schema');
 
 const app = express();
+
+app.use(cors())
 
 mongoose.connect('mongodb+srv://abhi:abhi1234@cluster0-qqz6h.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true })
 mongoose.connection.once('open', () => {
